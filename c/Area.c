fc405da9c70f8ed304721a1f850da306a657186b
Nuwan128 <nuwanpkumara128@gmail.com>
2021-10-28 09:56:03 +0530

    #include<stdio.h>

--- c/Area.c
+++ c/Area.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<math.h>
 
-int main() {
-int rad; float PI = 3.14;
-  
-printf("\nEnter radius of circle: ");
-scanf("%d", &rad);
-printf("\nArea of circle : %f ",  PI * pow(rad,2.0)); // formula to find area of circle
+#define PI 3.141
 
-printf("\nCircumference: %f ",  2 * PI * rad); // formula for Circumference of circle
-return (0);
+int main(){
+    float radius, area;
+    printf("Enter radius of circle\n");
+    scanf("%f", &radius);
+    /* Area of Circle = (PI * Radius * Radius) */
+    area = PI*pow(radius,2);
+    printf("Area of circle : %0.4f\n", area);
+    return 0;
 }
